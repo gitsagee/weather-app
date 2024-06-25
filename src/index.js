f506f5d5cd6eccd0199e492ba648c7d36c183a5c
Utsav <utsavjhaa2003@gmail.com>
2024-06-25 23:37:06 +0530

    port change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 
 app.set("views", path.join(__dirname, "..", "views"));
